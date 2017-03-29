81a27f799aa1404f2c3391d373198faed2796606
Pete D <peted70@hotmail.com>
2017-03-29 20:47:35 +0100

    added holographic remoting nuget package

---
+++ RemoteRender/RemoteRender/HolographicRemoteConnection.cpp
@@ -0,0 +1,106 @@
+#include "pch.h"
+#include "HolographicRemoteConnection.h"
+#include "DebugLog.h"
+#include <functional>
+
+HolographicRemoteConnection::HolographicRemoteConnection(const shared_ptr<DeviceResources>& deviceResources) :
+	m_deviceResources(deviceResources)
+{
+}
+
+void HolographicRemoteConnection::Connect(String^ ipAddress,
+	function<void()> OnConnected,
+	function<void(HolographicStreamerConnectionFailureReason)> OnDisconnected,
+	function<void(const ComPtr<ID3D11Texture2D>&)> OnPreviewFrame)
+{
+	m_streamerHelpers = ref new HolographicStreamerHelpers();
+	m_streamerHelpers->CreateStreamer(m_deviceResources->GetD3DDevice());
+
+	// We currently need to stream at 720p because that's the resolution of our remote display.
+	// There is a check in the holographic streamer that makes sure the remote and local
+	// resolutions match. The default streamer resolution is 1080p.
+	m_streamerHelpers->SetVideoFrameSize(1280, 720);
+
+	ConnectHandlers(ipAddress, m_streamerHelpers, OnConnected, OnDisconnected, OnPreviewFrame);
+
+	try
+	{
+		m_streamerHelpers->Connect(ipAddress->Data(), 8001);
+	}
+	catch (Exception^ ex)
+	{
+		DebugLog(L"Connect failed with hr = 0x%08X", ex->HResult);
+	}
+}
+
+void HolographicRemoteConnection::ConnectHandlers(String^ ipAddress,
+	HolographicStreamerHelpers^ helper,
+	function<void()> OnConnected, function<void(HolographicStreamerConnectionFailureReason)> OnDisconnected, 
+	function<void(const ComPtr<ID3D11Texture2D>&)> OnPreviewFrame)
+{
+	helper->OnConnected += ref new ConnectedEvent(OnConnected);
+
+	Platform::WeakReference streamerHelpersWeakRef = Platform::WeakReference(m_streamerHelpers);
+	helper->OnDisconnected += ref new DisconnectedEvent(
+		[this, streamerHelpersWeakRef, OnDisconnected, ipAddress]
+			(_In_ HolographicStreamerConnectionFailureReason failureReason)
+	{
+		DebugLog(L"Disconnected with reason %d", failureReason);
+
+		OnDisconnected(failureReason);
+
+		// Reconnect if this is a transient failure.
+		if (failureReason == HolographicStreamerConnectionFailureReason::Unreachable ||
+			failureReason == HolographicStreamerConnectionFailureReason::ConnectionLost)
+		{
+			DebugLog(L"Reconnecting...");
+
+			try
+			{
+				auto helpersResolved = streamerHelpersWeakRef.Resolve<HolographicStreamerHelpers>();
+				if (helpersResolved)
+				{
+					helpersResolved->Connect(ipAddress->Data(), 8001);
+				}
+				else
+				{
+					DebugLog(L"Failed to reconnect because a disconnect has already occurred.\n");
+				}
+			}
+			catch (Platform::Exception^ ex)
+			{
+				DebugLog(L"Connect failed with hr = 0x%08X", ex->HResult);
+			}
+		}
+		else
+		{
+			DebugLog(L"Disconnected with unrecoverable error, not attempting to reconnect.");
+		}
+	});
+
+	helper->OnSendFrame += ref new SendFrameEvent(
+		[this, OnPreviewFrame](_In_ const ComPtr<ID3D11Texture2D>& spTexture, _In_ FrameMetadata metadata)
+	{
+		OnPreviewFrame(spTexture);
+
+		//if (m_showPreview)
+		//{
+		//	ComPtr<ID3D11Device1> spDevice = m_deviceResources->GetD3DDevice();
+		//	ComPtr<ID3D11DeviceContext> spContext = m_deviceResources->GetD3DDeviceContext();
+
+		//	ComPtr<ID3D11Texture2D> spBackBuffer;
+		//	ThrowIfFailed(m_swapChain->GetBuffer(0, IID_PPV_ARGS(&spBackBuffer)));
+
+		//	spContext->CopySubresourceRegion(
+		//		spBackBuffer.Get(), // dest
+		//		0,                  // dest subresource
+		//		0, 0, 0,            // dest x, y, z
+		//		spTexture.Get(),    // source
+		//		0,                  // source subresource
+		//		nullptr);           // source box, null means the entire resource
+
+		//	DXGI_PRESENT_PARAMETERS parameters = { 0 };
+		//	ThrowIfFailed(m_swapChain->Present1(1, 0, &parameters));
+		//}
+	});
+};
---
+++ RemoteRender/RemoteRender/HolographicRemoteConnection.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <HolographicStreamerHelpers.h>
+
+using namespace Microsoft::Holographic;
+using namespace std;
+using namespace DX;
+using namespace Platform;
+using namespace Microsoft::WRL;
+
+// Helper class to wrap up the details of making a connection to a HoloLens device
+// listening at the IP address supplied. Must be used from the main render thread.
+// Device Resources should be those used for the main user interface..
+class HolographicRemoteConnection
+{
+public:
+	HolographicRemoteConnection(const std::shared_ptr<DeviceResources>& deviceResources);
+
+	void Connect(String^ ipAddress,
+		function<void()> OnConnected,
+		function<void(HolographicStreamerConnectionFailureReason)> OnDisconnected,
+		function<void(const ComPtr<ID3D11Texture2D>&)> OnPreviewFrame);
+
+private:
+	void ConnectHandlers(String^ ipAddress,
+		HolographicStreamerHelpers ^ helper,
+		function<void()> OnConnected, 
+		function<void(HolographicStreamerConnectionFailureReason)> OnDisconnected,
+		function<void(const ComPtr<ID3D11Texture2D>&)> OnPreviewFrame);
+
+	HolographicStreamerHelpers^ m_streamerHelpers;
+	shared_ptr<DeviceResources> m_deviceResources;
+};
+
